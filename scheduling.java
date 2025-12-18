67fe1673a36dc3abc200d199706cbdd95af25f9b
marwan hussein <marawanlabana649@gmail.com>
2025-12-18 13:04:21 +0200

    Make Independent Branch

--- scheduling.java
+++ scheduling.java
@@ -107,43 +107,6 @@ abstract class Schedule{
     }
 }
 
-//  ==============================================     AG    ============================================    //
-
-
-class ag_process extends Process {
-
-    protected int quantum = 0;
-    protected List<Integer> quantum_hist = new ArrayList<>();
-
-    public ag_process(String name, int arrival_time, int burst_time, int priority, int quantum) {
-
-        super(name, arrival_time, burst_time, priority);
-    }
-
-    public int get_quantum() {
-
-        return quantum;
-    }
-
-    public List<Integer> get_qunatm_hist() {
-
-        return quantum_hist;
-
-    }
-
-    public void set_quantum(int quantum) {
-
-        this.quantum = quantum;
-
-    }
-
-    public void add_to_qunatm_hist(int quantum) {
-
-        quantum_hist.add(quantum);
-
-    }
-}
-
 //  ==============================================     SRJF    ============================================    //
 
 class SJF_process extends Process {
@@ -193,67 +156,4 @@ class SJF_Schedule extends Schedule {
 
     @Override
     protected void calculateMetrics() {} // already calculated
-}
-
-
-public class scheduling {
-
-    List<ag_process> processes = new ArrayList<>();
-    List<ag_process> arrived_processes = new ArrayList<>();
-
-    public void ag_schedule() {
-
-        int current_time = 0;
-        List<String> execution_order = new ArrayList<>();
-
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("How many processes do you want to schedule: ");
-            int num_of_processes = sc.nextInt();
-            sc.nextLine();
-            for (int i = 0; i < num_of_processes; i++) {
-
-                System.out.println("Process " + i + 1 + " name: ");
-                String name = sc.next();
-
-                System.out.println("Arrival time: ");
-                int arrival = sc.nextInt();
-
-                System.out.println("burst time: ");
-                int burst = sc.nextInt();
-
-                System.out.println("priority: ");
-                int priority = sc.nextInt();
-
-                System.out.println("quantum: ");
-                int quantum = sc.nextInt();
-
-                ag_process p = new ag_process(name, arrival, burst, priority, quantum);
-                processes.add(p);
-
-            }
-            processes.sort(java.util.Comparator.comparingInt(p -> p.arrival_time));
-        }
-
-        while (!processes.isEmpty() || !arrived_processes.isEmpty()) {
-
-            for (ag_process p : processes) {
-
-                if (p.get_arrival_time() >= current_time) {
-                    arrived_processes.add(p);
-                }
-            }
-            if (!arrived_processes.isEmpty()) {
-
-                ag_process curr_p = arrived_processes.get(0);
-                curr_p.set_time_in(current_time);
-                curr_p.add_to_waiting_time();
-                int time_spent = (int) Math.ceil(curr_p.get_quantum() / 0.25);
-                current_time += time_spent;
-                int curr_p_quantum = curr_p.get_quantum();
-                curr_p.add_to_qunatm_hist(curr_p_quantum);
-                curr_p.set_quantum(curr_p_quantum - time_spent);
-            }
-        }
-
-    }
 }
